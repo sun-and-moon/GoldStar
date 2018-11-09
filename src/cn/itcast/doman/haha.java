61c333dda311a39b497c152ecd4e1c6d07966dbd
lsl <lslzcd205@126.com>
2018-11-09 19:58:52 +0800

    222

---
+++ src/cn/itcast/doman/haha.java
@@ -0,0 +1,13 @@
+package cn.itcast.doman;
+
+public class haha {
+    public static void main(String[] args) {
+        System.out.println(111111);
+        System.out.println(111111);
+        System.out.println(111111);
+        System.out.println(111111);
+        System.out.println(111111);
+        System.out.println(111111);
+        System.out.println(111111);
+    }
+}
--- src/cn/itcast/doman/heihei.java
+++ src/cn/itcast/doman/heihei.java
@@ -1,6 +1,9 @@
 package cn.itcast.doman;
 
+import java.sql.SQLOutput;
+
 public class heihei {
 
     int s = 10;
+    int b = 20;
 }
